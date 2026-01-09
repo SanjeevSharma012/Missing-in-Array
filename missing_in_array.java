b60df51d268429cd161698f95b49d2a20c2f2b4a
Sanjeev_Sharma <sharmasanj99717@gmail.com>
2026-01-09 23:55:23 +0530

    Add missing number detection in array
    Implement method to find the missing number in a sorted array.

---
+++ missing_in_array.java
@@ -0,0 +1,17 @@
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        Arrays.sort(arr);
+        int count = 1 ;
+        
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i] == count){
+                count++;
+            }else {
+                return count ;
+                
+            }
+        }
+        return count ;
+    }
+}
