6b57ed96ff040bb4d3ff52bb3816d5cd3c9e2131
Sanjeev_Sharma <sharmasanj99717@gmail.com>
2026-03-15 22:19:18 +0530

    Update missing_in_array.java

--- missing_in_array.java
+++ missing_in_array.java
@@ -31,3 +31,28 @@ class Solution {
         return (int)ans;
     }
 }
+
+3.Solution
+    class Solution {
+    int missingNum(int arr[]) {
+        // code here
+       int n = arr.length + 1 ;
+       int i = 0 ;
+       while(i < arr.length){
+           if(arr[i] == i + 1 || arr[i] == n) i++;
+           else {
+               int idx = arr[i] - 1 ;
+               swap(arr,i,idx);
+           }
+       }
+       for(i = 0 ; i < arr.length ; i++){
+           if(arr[i] != i + 1) return i + 1; 
+       }
+       return n ;
+    }
+    void swap(int[] arr ,int i , int idx){
+        int temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp ;
+    }
+}
